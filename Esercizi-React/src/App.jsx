a2373db3fc4ea0de87aa5a527fa87eb39a89c71d
Danichiesa03 <edoardochiesa3@gmail.com>
2025-03-31 00:23:49 +0200

    d

--- Esercizi-React/src/App.jsx
+++ Esercizi-React/src/App.jsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
 import "./App.css";
+import "./components/Color";
+import Colors from "./components/Colors";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Colors
+        colors={[
+          { id: 2, name: "Brown" },
+          { id: 3, name: "Purple" },
+          { id: 4, name: "Green" },
+          { id: 5, name: "Black" },
+        ]}
+      ></Colors>
+    </>
+  );
 }
 
 export default App;
