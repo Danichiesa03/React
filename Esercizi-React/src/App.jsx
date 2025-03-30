ac26b0be6d82aaf1bbe1161aea4bb1df602a5651
Danichiesa03 <edoardochiesa3@gmail.com>
2025-03-31 00:03:46 +0200

    d

--- Esercizi-React/src/App.jsx
+++ Esercizi-React/src/App.jsx
@@ -1,8 +1,48 @@
 import { useState } from "react";
 import "./App.css";
+import useCounter from "./components/Counter";
+import useInput from "./components/Input";
 
-function App() {
-  return <></>;
+function App({ initialValue = 0 }) {
+  const { count, increment, decrement, reset } = useCounter(initialValue);
+  const { formData, handleChange } = useInput();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Username:", formData.username);
+    console.log("Password:", formData.password);
+  };
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={increment}>Incrementa</button>
+      <button onClick={decrement}>Decrementa</button>
+      <button onClick={reset}>Resetta</button>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
 }
 
 export default App;
---
+++ Esercizi-React/src/components/Input.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function useInput() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  return { formData, handleChange };
+}
+export default useInput;
