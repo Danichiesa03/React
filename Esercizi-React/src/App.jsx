9d72ae1c51aa54dc44c121289e69b7fd397d04ff
Danichiesa03 <edoardochiesa3@gmail.com>
2025-03-31 00:40:09 +0200

    d

--- Esercizi-React/src/App.jsx
+++ Esercizi-React/src/App.jsx
@@ -1,8 +1,25 @@
 import { useState } from "react";
 import "./App.css";
 
+import { LanguageContext } from "./components/LanguageContext";
+import Hello from "./components/Hello";
+
 function App() {
-  return <></>;
+  const [language, setLanguage] = useState("en");
+
+  function handleLanguage(event) {
+    setLanguage(event.target.value);
+  }
+
+  return (
+    <LanguageContext.Provider value={{ language, handleLanguage }}>
+      <select value={language} onChange={handleLanguage}>
+        <option value="it">IT</option>
+        <option value="en">EN</option>
+      </select>
+      <Hello />
+    </LanguageContext.Provider>
+  );
 }
 
 export default App;
