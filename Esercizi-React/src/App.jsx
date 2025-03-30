a9ab87124b4d5792421235f0b60359dc2c82b940
Danichiesa03 <edoardochiesa3@gmail.com>
2025-03-30 15:37:17 +0200

    d

--- Esercizi-React/src/App.jsx
+++ Esercizi-React/src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import useCounter from "./components/Counter";
 
-function App() {
-  return <></>;
+function App({ initialValue = 0 }) {
+  const { count, increment, decrement, reset } = useCounter(initialValue);
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={increment}>Incrementa</button>
+      <button onClick={decrement}>Decrementa</button>
+      <button onClick={reset}>Resetta</button>
+    </>
+  );
 }
 
 export default App;
